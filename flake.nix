8c930a6cdb1f714a59ce854169f196d1440a1e44
Will Darrragh <wdarragh16@gmail.com>
2025-01-14 20:27:35 -0600

    Added nixos-cosmic to flake

--- flake.nix
+++ flake.nix
@@ -5,12 +5,22 @@
   inputs = {
     # NixOS official package source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    # Cosmic desktop
+    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.arrakis = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        {
+          nix.settings = {
+	    substituters = [ "https://cosmic.cachix.org/" ];
+            trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBenWjrkPv6rtxpmMdRe102smYzA85dPE=" ];
+          }
+        }
+        nix-cosmic.nixosModules.default
         # Import configuration.nx
         ./configuration.nix
       ];
