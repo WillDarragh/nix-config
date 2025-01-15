cb2387ea9b73be23c12372429d9a97817c63869a
Will Darrragh <wdarragh16@gmail.com>
2025-01-14 20:35:26 -0600

    Enabling cosmic

--- flake.nix
+++ flake.nix
@@ -24,6 +24,8 @@
         # Import configuration.nx
         ./configuration.nix
       ];
+      services.desktopManager.cosmic.enable = true;
+      services.displayManager.cosmic-greeter.enable = true;
     };
   };
 }
