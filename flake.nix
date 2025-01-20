c7ec447373a209415f78519abd291386c11accaa
Will Darragh <wdarragh16@gamail.com>
2025-01-19 22:05:31 -0600

    Removed cosmic references

--- flake.nix
+++ flake.nix
@@ -3,32 +3,26 @@
   description = "There is no escape--We pay for the violence of our ancestors.";
 
   inputs = {
+    
     # NixOS official package source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    
     # Home Mangaer
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       # 'follow' is for fancy inheritance
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Cosmic desktop
-    #nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
   };
 
-  outputs = { self, nixpkgs, home-manager, /*nixos-cosmic,*/ ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    
+    # Arrakis (Small Form Factor Desktop)
     nixosConfigurations.arrakis = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        /*{
-          nix.settings = {
-	    substituters = [ "https://cosmic.cachix.org/" ];
-            trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBenWjrkPv6rtxpmMdRe102smYzA85dPE=" ];
-          };
-        }
-        nixos-cosmic.nixosModules.default
-        */
 
-        # Import configuration.nix
+        # System configuration
         ./hosts/arrakis/configuration.nix
 
         # Make home-manager module of nixos
@@ -36,24 +30,18 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
-          # User
+          # Home-manager
           home-manager.users.will = import ./home/will.nix;
         }
       ];
     };
+
+    # Caladan (System76 LemurPro Laptop)
     nixosConfigurations.caladan = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        /*{
-          nix.settings = {
-	    substituters = [ "https://cosmic.cachix.org/" ];
-            trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBenWjrkPv6rtxpmMdRe102smYzA85dPE=" ];
-          };
-        }
-        nixos-cosmic.nixosModules.default
-        */
 
-        # Import configuration.nix
+        # System configuration
         ./hosts/caladan/configuration.nix
 
         # Make home-manager module of nixos
@@ -61,7 +49,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
-          # User
+          # Home-manager
           home-manager.users.will = import ./home/will.nix;
         }
       ];
