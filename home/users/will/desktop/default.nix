59c735b88a38465e94e9c53adee08f1df347cbb7
Will Darrragh <wdarragh16@gmail.com>
2025-02-12 17:58:17 -0600

    Firefox config

--- home/users/will/desktop/default.nix
+++ home/users/will/desktop/default.nix
@@ -23,9 +23,31 @@
     profiles = {
       default = {
         name = "default";
+        id = 0;
+        isDefault = true;
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          privacy-badger
+          ublock-origin
+          bitwarden
+          proton-pass
+          darkreader
+          vimium
+          facebook-container
         ];
+        bookmarks = [
+          {
+            name = "Test";
+            #tags = [""];
+            #keyword = "";
+            url = "www.google.com";
+            #toolbar = true;
+            #bookmarks = [];
+          }
+        ];
+        search.default = "DuckDuckGo";
+        search.force = true;
+        settings = {
+          "layout.css.devPixelsPerPx" = 1.5;
+        };
       };
     };
   };
